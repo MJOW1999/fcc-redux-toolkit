c75df5c11c89bf2d297f5146592f376e9f655497
Michael Williams <michael.williams9@ticketmaster.co.uk>
2023-03-18 18:18:21 +0000

    fcc-redux-toolkit: Better practice calculateTotals
    - Move logic to App.js and use a useEffect hook to check when the number of cartItems changes
    
    [demo] [e2e]

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { calculateTotals } from "./features/cart/cartSlice";
 
 function App() {
+  const { cartItems } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems, dispatch]);
+
   return (
     <main>
       <Navbar />
--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,10 +1,5 @@
 import { ChevronDown, ChevronUp } from "../icons";
-import {
-  removeItem,
-  increase,
-  decrease,
-  calculateTotals,
-} from "../features/cart/cartSlice";
+import { removeItem, increase, decrease } from "../features/cart/cartSlice";
 import { useDispatch } from "react-redux";
 
 const CartItem = ({ id, img, title, price, amount }) => {
@@ -24,7 +19,6 @@ const CartItem = ({ id, img, title, price, amount }) => {
           className="amount-btn"
           onClick={() => {
             dispatch(increase({ id }));
-            dispatch(calculateTotals());
           }}
         >
           <ChevronUp />
@@ -35,11 +29,10 @@ const CartItem = ({ id, img, title, price, amount }) => {
           onClick={() => {
             if (amount === 1) {
               dispatch(removeItem(id));
-              dispatch(calculateTotals());
+
               return;
             }
             dispatch(decrease({ id }));
-            dispatch(calculateTotals());
           }}
         >
           <ChevronDown />
